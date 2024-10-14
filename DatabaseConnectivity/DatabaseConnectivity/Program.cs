f007c72e2f83ecc091b24a89f02c18143764b748
jospp <68458112+josspp@users.noreply.github.com>
2024-10-14 16:53:14 +0530

    new1
    new1

---
+++ DatabaseConnectivity/DatabaseConnectivity/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DatabaseConnectivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SQL Server Connection String
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=SampleDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            //Fetch Query
+            string sqlQuery = "SELECT Id,Name,Email FROM Users";
+
+            //List of users
+            List<User> usersList = new List<User>();
+
+            //Connecting to SQL server
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //Creating a connection
+                SqlCommand command = new SqlCommand(sqlQuery, connection);
+                Console.WriteLine("Connecting To Database..");
+
+
+                //Establish Connection
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Successfully connected to Database!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Connecting Database..\n" + ex.Message);
+                }
+
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    //Execute the query and fetching data
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        //Read Data Until The End Of Data
+                        Console.Write("Getting Data From Database..");
+                        while (reader.Read())
+                        {
+                            //Assgin The Query Results Into Class Object
+                            User user = new User
+                            {
+                                Id = reader.GetInt32(0), //Id As First Column
+                                Name = reader.GetString(1), //Name As Second Column
+                                Email = reader.GetString(2) //Email As Third Column
+                            };
+
+                            //Adding User Into List
+                            usersList.Add(user);
+                            Console.Write(".");
+                        }
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            //Loop through the list of users and print
+            Console.WriteLine("_________________________________________");
+            foreach (var user in usersList)
+            {
+                Console.WriteLine($"Id: {user.Id}\t Name: {user.Name}\t Email: {user.Email}");
+            }
+        }
+    }
+}
---
+++ SeleniumLogin/Program.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumLogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selenium Login started");
+            IWebDriver driver = new ChromeDriver();
+            string userName = "student";
+            string password = "Password123";
+
+            //Navigating to the url
+            Console.WriteLine("Navigating to the test url");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");      
+            
+
+            //Typing user name and password
+            Console.WriteLine("Typing user name and password");
+            IWebElement UsernameField = driver.FindElement(By.Id("username"));
+            IWebElement PasswordField = driver.FindElement(By.Id("password"));
+            UsernameField.SendKeys(userName);
+            PasswordField.SendKeys(password);
+
+            //Using jQuery to locate submit button, click
+            Console.WriteLine("Using jQuery to locate submit button");
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            string script = "document.getElementById('submit').click();";
+  
+            //Clicking login button jQuery
+            Console.WriteLine("Clicking login button");
+            IWebElement LoginButton = (IWebElement)js.ExecuteScript(script);
+
+            //Check login success or not
+            Console.WriteLine("Checking login success or not by element(XPath)");
+            //Using xpath to locate element - Success login label
+            IWebElement LoginSuccessLable = driver.FindElement(By.XPath("//h1[@class='post-title']"));
+
+            if(LoginSuccessLable.Text == "Logged In Successfully")
+            {
+                Console.WriteLine("Successfully logged in");
+            }
+            else
+            {
+                Console.WriteLine("Login failed");
+            }
+            
+            Console.WriteLine("Selenium Login completed");
+        }
+    }
+}
